c9d39ec9ef82b27805cf3fbd676fceef9f6b6dd3
Daniel Soromou <foromodanielsoromou@gmail.com>
2025-07-13 02:01:41 -0400

    Maximum Subarray Sum

---
+++ sorting-and-searching/maximum-subarray-sum-1643.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <numeric>
+#include <limits>
+#include <functional>
+#include <cmath>
+#include <unordered_set>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+
+template<class T> using Vec   = vector<T>;
+template<class T> using MinPQ = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i,n)        for (int i = 0; (i) < (n); ++(i))
+#define rep1(i,a,b)     for (int i = (a); (i) <= (b); ++(i))
+#define repd(i,n)       for (int i = (n)-1; (i) >= 0;  --(i))
+#define all(x)          begin(x), end(x)
+#define rall(x)         rbegin(x), rend(x)
+#define sz(x)           (int)((x).size())
+#define pb              push_back
+#define eb              emplace_back
+
+constexpr int  INF   = 1e9+5;
+constexpr ll   INF64 = (1LL<<60);
+constexpr int  MOD   = 1'000'000'007;
+
+inline ll mod_pow(ll a, ll e, ll m = MOD) {
+    ll res = 1;
+    while (e) { if (e & 1) res = res * a % m; a = a * a % m; e >>= 1; }
+    return res;
+}
+inline ll mod_inv(ll a, ll m = MOD) { return mod_pow(a, m-2, m); }
+
+template<class T> bool chmin(T& a, const T& b){ return b < a ? (a = b, true) : false; }
+template<class T> bool chmax(T& a, const T& b){ return b > a ? (a = b, true) : false; }
+
+#ifdef LOCAL
+  #define dbg(...) cerr << " [" << #__VA_ARGS__ << "] = ", _dbg(__VA_ARGS__)
+  template<class T> void _dbg(const T& t){ cerr << t << '\n'; }
+  template<class H, class... Ts> void _dbg(const H& h, const Ts&... ts){ cerr << h << ", "; _dbg(ts...); }
+#else
+  #define dbg(...) (void)0
+#endif
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n;
+    cin >> n;
+    Vec<ll> arr(n);
+    ll sum = 0;
+    ll best = numeric_limits<ll>::min();
+    for (ll i = 0; i < n; ++i) {
+        ll a;
+        cin >> a;
+        sum = max(a, sum + a);
+        best = max(best, sum);
+    }
+    cout << best << endl;
+    return 0;
+}
